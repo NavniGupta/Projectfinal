8aa06631588ca351ceab50b388872fdbbd2545a3
NavniGupta <43049801+NavniGupta@users.noreply.github.com>
2018-12-08 13:49:35 -0500

    Update prj-ece650.cpp

--- prj-ece650.cpp
+++ prj-ece650.cpp
@@ -239,7 +239,7 @@ void * APPROX_VC2(void *input)
     }
 
     // Print the vertex cover
-    for (unsigned int j=0; j<n: j++)
+    for (unsigned int j=0; j< n; j++)
     {
          if (visited[j])
         C.push_back(visited[j]);
